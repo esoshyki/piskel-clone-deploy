c8ac1ddfd59611d27615aab4f2838cf929295e0c
esoshyki <esoshyki@yandex.by>
2019-11-09 09:46:33 +0300

    feat: finish pencil instrument

--- codejam-canvas/src/js/artist.js
+++ codejam-canvas/src/js/artist.js
@@ -3,11 +3,13 @@ export default class Artist {
         this.instrument = 'pencil';
         this.canvas = Canvas;
         this.ctx = this.canvas.getContext('2d');
-        this.ctx.fillstyle = 'black';
+        this.ctx.fillstyle = 'rgba(0,0,0,255)';
+        this.currentColor = [0,0,0,255];
         this.previousColor = 'red';
         this.body = document.querySelector('._artist')
         this.handleClick = this.handleClick.bind(this)
         this.draw = this.draw.bind(this);
+        this.drawPath = this.drawPath.bind(this);
         this.selectedNode = this.body.querySelector('[name=pencil]');
         this.ratio = 4;
     }
@@ -43,20 +45,39 @@ export default class Artist {
 
     }
 
+
     draw(e) {
 
         if (!this.instrument) return // ничего не делает если инструмент не выбран
-        const x = e.pageX - this.canvas.offsetLeft;
-        const y = e.pageY - this.canvas.offsetTop;
-        const square = this.getSquare(x, y)
-        if (this.instrument === 'pencil') this.pencilDraw(square)
+
+        const square = this.getSquare(e)
+
+        if (this.instrument === 'pencil') this.pencilDraw(square);
+
+        this.canvas.addEventListener('mousemove', this.drawPath)
+        this.canvas.addEventListener('mouseup', () => this.canvas.removeEventListener('mousemove', this.drawPath))
+      }
+
+    drawPath(e) {
+        const square = this.getSquare(e)
+        if (this.checkColor(square)) return
+        else {
+            this.pencilDraw(square)
+        }
+    }
+    
+    checkColor(square) {
+        const c = [...this.ctx.getImageData(square[0]+2,square[1]+2,1,1).data];
+        return `${c}`=== `${this.currentColor}`
     }
 
     pencilDraw(square) {
         this.ctx.fillRect(square[0],square[1], square[2], square[2])
     }
 
-    getSquare(x, y) {
+    getSquare(e) {
+        const x = e.pageX - this.canvas.offsetLeft;
+        const y = e.pageY - this.canvas.offsetTop;
         const size = 512 / this.ratio;
         const startX = size * Math.floor(x/size);
         const startY = size * Math.floor(y/size);
